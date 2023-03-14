81ac57d16685654f73eaaf025adf53e153f2dc6d
peterudu <puduevborho7@gmail.com>
2023-03-14 21:06:18 +0000

    Create Sidebar component and add div containing 3 <a> tags

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Sidebar() {
+    return <div>
+        <a href="#">My Photos</a>
+        <a href="#">My Illustratiions</a>
+        <a href="#">My Paintings</a>
+    </div>
+}
+
+export default Sidebar;
